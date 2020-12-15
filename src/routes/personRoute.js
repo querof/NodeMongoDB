f95995d16b63a4a4ea9e60cc75035a7417e6e4c6
Frank Quero <querof@gmail.com>
2020-12-15 11:06:37 +0100

    [NMDB-006] Adding CRUD and http calls

--- src/routes/personRoute.js
+++ src/routes/personRoute.js
@@ -1,31 +1,77 @@
-const {
-    Person
-} = require('../repositories/personRepository');
+const Person = require('../repositories/personRepository');
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    return res.send({
-        route: 'Root route'
-    });
+router.get('/', async (req, res) => {
+    try {
+        const persons = await Person.find();
+        res.json(persons);
+    } catch (err) {
+        res.json({
+            message: err
+        });
+    }
 });
 
-router.post('/', (req, res) => {
-    return res.send({
-        body: req.body.name + ' ' + req.body.lastname
-    });
+router.get('/:id', async (req, res) => {
+    try {
+        const person = await Person.findById(req.params.id);
+        if (person === null) {
+            return res.json({
+                message: "no record with Id: " + req.params.id
+            });
+        }
+        res.json(person);
+    } catch (err) {
+        res.json({
+            message: err
+        });
+    }
 });
 
-router.put('/:id', (req, res) => {
-    return res.send({
-        body: req.body.name + ' adding characters'
+router.post('/', async (req, res) => {
+    const person = new Person({
+        name: req.body.name,
+        lastname: req.body.lastname
     });
+
+    try {
+        const savedPerson = await person.save();
+        res.json(savedPerson);
+    } catch (err) {
+        res.json({
+            message: err
+        });
+    }
 });
 
-router.delete('/:id', (req, res) => {
-    return res.send({
-        body: 'deleted person with id: ' + req.params.id
-    });
+router.put('/:id', async (req, res) => {
+    try {
+        const person = await Person.updateOne({
+            _id: req.params.id
+        }, {
+            name: req.body.name
+        });
+
+        res.json(person);
+    } catch (err) {
+        res.json({
+            err
+        });
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const post = await Person.remove({
+            _id: req.params.id
+        });
+        return res.json(post);
+    } catch (err) {
+        res.json({
+            err
+        });
+    }
 });
 
 module.exports = router;
